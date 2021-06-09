81337abfdd22dfee006624f8a50d4bad9961ebb9
Taylan YILDIZ <taylanyildz@outlook.com>
2021-06-09 16:22:21 +0300

    update something

--- lib/constants/mqtt_constant.dart
+++ lib/constants/mqtt_constant.dart
@@ -1,6 +1,6 @@
 class MqttConstant {
   /// Mqtt Host Adress
-  static const String MQTT_HOST_ADRESS = '----';
+  static const String MQTT_HOST_ADRESS = '77.245.151.85';
 
   /// Mqtt Port Adress
   static const String MQTT_PORT_ADRESS = '1883';
